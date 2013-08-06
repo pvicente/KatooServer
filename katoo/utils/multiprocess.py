645950f0b0b1c7a4050fefba0293f6db34dc86bd
Pedro Vicente Fernandez <pedrovfer@gmail.com>
2013-08-07 01:23:41 +0200

    Added MultiProcess as service and removed from init file

---
+++ katoo/utils/multiprocess.py
@@ -0,0 +1,63 @@
+'''
+Created on Aug 7, 2013
+
+@author: pvicente
+'''
+from applog import getLogger, getLoggerAdapter
+from twisted.application import service
+from twisted.internet import reactor
+from twisted.internet.protocol import ProcessProtocol
+import os
+
+log = getLogger(__name__, level='INFO')
+
+class MultiProcessProtocol(ProcessProtocol):
+    def __init__(self, parent):
+        self.log = getLoggerAdapter(log, id=self.__class__.__name__)
+        self.parent = parent
+        self.parent.childs.append(self)
+        self.exit = False
+        self.ended = False
+    
+    def connectionMade(self):
+        self.log.info("connectionMade!")
+    
+    def outReceived(self, data):
+        self.log.info("outReceived! with %d bytes!\n%r", len(data), data)
+
+    def errReceived(self, data):
+        self.log.info("errReceived! with %d bytes!\n%r", len(data), data)
+    
+    def inConnectionLost(self):
+        self.log.info("inConnectionLost! stdin is closed! (we probably did it)")
+    
+    def outConnectionLost(self):
+        self.log.info("outConnectionLost! The child closed their stdout!")
+    
+    def errConnectionLost(self):
+        self.log.info("errConnectionLost! The child closed their stderr")
+    
+    def processExited(self, reason):
+        self.log.info("processExited, status %s", reason)
+        self.exit = True
+    
+    def processEnded(self, reason):
+        self.log.info("processEnded, status %s", reason)
+        self.ended = True
+
+
+class MultiProcess(service.Service):
+    def __init__(self, pythontacfile, number=1, fds=[]):
+        self.childFDs = dict([(0,0), (1,1), (2,2)] + [(fd,fd) for fd in fds])
+        self.command = pythontacfile
+        self.procnumbers = number
+        self.childs = []
+        
+    def startService(self):
+        for i in xrange(self.procnumbers):
+            reactor.spawnProcess(MultiProcessProtocol(self), 'twistd', ['twistd', '-ny', self.command, '--pidfile=%s-%s.pid'%(self.command, i)], env=os.environ, childFDs=self.childFDs)
+    
+    def stopService(self):
+        for child in self.childs:
+            child.transport.signalProcess('TERM')
+        #TODO: pending to test exit and send KILL to ensure death
--- runmultiprocess.py
+++ runmultiprocess.py
@@ -5,51 +5,10 @@ Created on Aug 6, 2013
 '''
 
 from katoo import KatooApp
-from twisted.application import service
-from twisted.internet import reactor
-from twisted.internet.protocol import ProcessProtocol
-import os
-
-class MultiProcessProtocol(ProcessProtocol):
-    def connectionMade(self):
-        print "connectionMade!"
-    def outReceived(self, data):
-        print "outReceived! with %d bytes!" % len(data)
-        print data
-    def errReceived(self, data):
-        print "errReceived! with %d bytes!" % len(data)
-        print data
-        print repr(data)
-    
-    def inConnectionLost(self):
-        print "inConnectionLost! stdin is closed! (we probably did it)"
-    def outConnectionLost(self):
-        print "outConnectionLost! The child closed their stdout!"
-    def errConnectionLost(self):
-        print "errConnectionLost! The child closed their stderr."
-    def processExited(self, reason):
-        print "processExited, status %s" % (reason.value.exitCode,)
-    def processEnded(self, reason):
-        print "processEnded, status %s" % (reason.value.exitCode,)
-        print "quitting"
-
-class MultiProcess(service.Service):
-    def __init__(self):
-        self.protocol = MultiProcessProtocol()
-        self.childs = []
-        
-    def startService(self):
-        for i in xrange(2):
-            self.childs.append(reactor.spawnProcess(self.protocol, 'twistd', ['twistd', '-ny', 'runxmpp.py', '--pidfile=xmpp%s.pid'%(i)], env=os.environ, childFDs={0:0, 1:1, 2:2}))
-    
-    def stopService(self):
-        for child in self.childs:
-            child.signalProcess('TERM')
-             
+from katoo.utils.multiprocess import MultiProcess
 
 application = KatooApp().app
 
-m = MultiProcess()
+m = MultiProcess('runxmpp.py')
 m.setServiceParent(application)
-
 
