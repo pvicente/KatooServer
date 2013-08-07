8d73f43740e569e53a97c47778f16e249b694ff9
Pedro Vicente Fernandez <pedrovfer@gmail.com>
2013-08-07 10:53:37 +0200

    Updated runall with multiprocess behaviour

--- runall.py
+++ runall.py
@@ -1,5 +1,5 @@
 '''
-Created on Jun 4, 2013
+Created on Aug 7, 2013
 
 @author: pvicente
 '''
@@ -8,18 +8,34 @@ from katoo.apns.api import KatooAPNSService
 from katoo.rqtwisted import worker
 from katoo.supervisor import LocalSupervisor, GlobalSupervisor
 from katoo.utils.applog import getLoggerAdapter, getLogger
+from katoo.utils.multiprocess import MultiProcess
 from katoo.web import app
-from twisted.application import internet
+from socket import AF_INET
+from twisted.internet import reactor
+import os
+
+#===============================================================================
+#from twisted.application import internet
+
+#Running cyclone as service in monocore. Deprecated with multiprocess support
+# webservice = internet.TCPServer(conf.PORT, app, interface=conf.LISTEN) 
+# webservice.setServiceParent(application)
+#===============================================================================
 
 application = KatooApp().app
-webservice = internet.TCPServer(conf.PORT, app, interface=conf.LISTEN) 
-webservice.setServiceParent(application)
 
-supervisor = LocalSupervisor()
-supervisor.setServiceParent(application)
-
-workers_supervisor = GlobalSupervisor()
-workers_supervisor.setServiceParent(application)
+if conf.ADOPTED_STREAM is None:
+    stream = reactor.listenTCP(port=conf.PORT, factory=app, backlog=conf.BACKLOG, interface=conf.LISTEN)
+    os.environ['ADOPTED_STREAM']=str(stream.fileno())
+    workers_supervisor = GlobalSupervisor()
+    workers_supervisor.setServiceParent(application)
+    supervisor = LocalSupervisor()
+    supervisor.setServiceParent(application)
+    if conf.MULTIPROCESS>0:
+        m=MultiProcess(__file__, number=conf.MULTIPROCESS, fds=[stream.fileno()])
+        m.setServiceParent(application)
+else:
+    reactor.adoptStreamPort(int(conf.ADOPTED_STREAM), AF_INET, app)
 
 KatooAPNSService().service.setServiceParent(application)
 
