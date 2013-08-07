175b88752cb6053794e0b148ad1020dea52a14d7
Pedro Vicente Fernandez <pedrovfer@gmail.com>
2013-08-07 14:31:16 +0200

    Ensure that all child processes are ended when service is stopped

--- katoo/utils/multiprocess.py
+++ katoo/utils/multiprocess.py
@@ -4,8 +4,9 @@ Created on Aug 7, 2013
 @author: pvicente
 '''
 from applog import getLogger, getLoggerAdapter
+from katoo.utils.time import sleep
 from twisted.application import service
-from twisted.internet import reactor
+from twisted.internet import reactor, defer
 from twisted.internet.protocol import ProcessProtocol
 import os
 
@@ -16,7 +17,6 @@ class MultiProcessProtocol(ProcessProtocol):
         self.log = getLoggerAdapter(log, id=self.__class__.__name__)
         self.parent = parent
         self.parent.childs.append(self)
-        self.exit = False
         self.ended = False
     
     def connectionMade(self):
@@ -39,29 +39,42 @@ class MultiProcessProtocol(ProcessProtocol):
     
     def processExited(self, reason):
         self.log.info("processExited, status %s", reason)
-        self.exit = True
+        self.ended = True
+        if reactor.running:
+            reactor.stop()
+            self.log.warning('Stopped reactor due to child process exited')
     
     def processEnded(self, reason):
         self.log.info("processEnded, status %s", reason)
         self.ended = True
+        if reactor.running:
+            reactor.stop()
+            self.log.warning('Stopped reactor due to child process ended')
 
 
 class MultiProcess(service.Service):
     def __init__(self, pythontacfile, number=1, fds=[]):
+        self.log = getLoggerAdapter(log, id=self.__class__.__name__)
         self.childFDs = dict([(0,0), (1,1), (2,2)] + [(fd,fd) for fd in fds])
         self.command = pythontacfile
         self.procnumbers = number
         self.childs = []
-        
+    
+    @defer.inlineCallbacks
+    def send_signal(self, signal):
+        for child in self.childs:
+            if not child.ended:
+                self.log.info('Sending %s to child process', signal)
+                yield child.transport.signalProcess(signal)
+    
     def startService(self):
         service.Service.startService(self)
+        self.log.info('Starting Service')
         for i in xrange(self.procnumbers):
             reactor.spawnProcess(MultiProcessProtocol(self), 'twistd', ['twistd', '-ny', self.command, '--pidfile=%s-%s.pid'%(self.command, i)], env=os.environ, childFDs=self.childFDs)
     
     def stopService(self):
         service.Service.stopService(self)
-        for child in self.childs:
-            if not child.ended:
-                child.transport.signalProcess('TERM')
-        
-        #TODO: pending to test exit and send KILL to ensure death
+        self.log.info('Stopping Service')
+        self.send_signal('TERM')
+    
