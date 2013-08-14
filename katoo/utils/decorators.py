66040a83d4fe90292143dc729d48a8b032ad4c71
Pedro Vicente Fernandez <pedrovfer@gmail.com>
2013-08-14 10:31:29 +0200

    Added inject_decorator to inject only one decorator to some methods

--- katoo/utils/decorators.py
+++ katoo/utils/decorators.py
@@ -15,3 +15,15 @@ def inject_decorators(method_decorator_dict):
                     setattr(cls, attr, method_decorator_dict[attr](tmp))
         return ret
     return decorate
+
+def inject_decorator(method_list, decorator):
+    methods = set(method_list)
+    def decorate(cls):
+        ret = cls
+        for cls in inspect.getmro(cls):
+            for attr in [v for v in cls.__dict__ if v in methods]:
+                tmp = getattr(cls,attr)
+                if callable(tmp):
+                    setattr(cls, attr, decorator(tmp))
+        return ret
+    return decorate
