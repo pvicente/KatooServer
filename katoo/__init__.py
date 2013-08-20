ca19e7e3dd352883b700fee2da13e24ed73e1ad9
Pedro Vicente Fernandez <pedrovfer@gmail.com>
2013-08-20 21:00:38 +0200

    Added mongo calls statistics

--- katoo/__init__.py
+++ katoo/__init__.py
@@ -1,11 +1,12 @@
 from katoo import conf
+from katoo.metrics import IncrementMetric
+from katoo.rqtwisted import job, worker
+from katoo.txMongoModel.mongomodel import model
 from katoo.utils.applog import TwistedLogging
 from katoo.utils.connections import RedisMixin, MongoMixin
+from katoo.utils.decorators import inject_decorators
 from katoo.utils.patterns import Singleton
 from twisted.application import service
-from katoo.rqtwisted import job, worker
-from katoo.utils.decorators import inject_decorators
-from katoo.metrics import IncrementMetric
 
 class KatooApp(Singleton):
     def constructor(self):
@@ -36,6 +37,16 @@ class KatooJob(job.Job):
 
 @inject_decorators(method_decorator_dict={'callback_perform_job': IncrementMetric(name='jobs_ok', unit='jobs', source='RQTWISTED'), 'errback_perform_job': IncrementMetric(name='jobs_failed', unit='jobs', source='RQTWISTED')})
 class KatooWorker(worker.Worker):
+    pass
+
+@inject_decorators(method_decorator_dict={'execute':  IncrementMetric(name='mongo_transaction', unit='calls', source='MONGO'), 
+                                          'insert':   IncrementMetric(name='mongo_insert', unit='calls', source='MONGO'),
+                                          'find_one': IncrementMetric(name='mongo_findone', unit='calls', source='MONGO'),
+                                          'find':     IncrementMetric(name='mongo_find', unit='calls', source='MONGO'),
+                                          'remove':   IncrementMetric(name='mongo_remove', unit='calls', source='MONGO'),
+                                          'update':   IncrementMetric(name='mongo_update', unit='calls', source='MONGO')
+                                          })
+class KatooDataModel(model.Model):
     pass
 
 KatooApp()
